561d9b0742d842924767c0da8221aa049b3edaad
Oleh Filyk <filykoleg2102@gmail.com>
2026-06-16 00:55:17 +0300

    feat(core): add StatusCheckRecord entity and IStatusRepository interface

---
+++ src/RegistrationMonitor.Core/Interfaces/IStatusRepository.cs
@@ -0,0 +1,25 @@
+﻿using RegistrationMonitor.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistrationMonitor.Core.Interfaces
+{
+    public interface IStatusRepository
+    {
+        /// <summary>
+        /// Get last check from db. Anti-spam mechanism
+        /// </summary>
+        /// <param name="token"></param>
+        Task<StatusCheckRecord> GetLastCheckAsync(CancellationToken token = default);
+
+        /// <summary>
+        /// Saves new check in db
+        /// </summary>
+        /// <param name="record"></param>
+        /// <param name="token"></param>
+        Task AddCheckAsync(StatusCheckRecord record, CancellationToken token = default);
+    }
+}
