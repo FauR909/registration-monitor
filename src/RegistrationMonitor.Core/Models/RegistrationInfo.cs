608e072c3222a3cb002861eaa6c8b63bdd099982
Oleh Filyk <filykoleg2102@gmail.com>
2026-06-15 19:58:33 +0300

    feat(infrastructure): implement DummyRegistrationTracker

--- src/RegistrationMonitor.Core/Models/RegistrationInfo.cs
+++ src/RegistrationMonitor.Core/Models/RegistrationInfo.cs
@@ -9,13 +9,13 @@ namespace RegistrationMonitor.Core.Models
     /// <summary>
     /// Result of checking registration status
     /// </summary>
-    /// <param name="status">Registration status</param>
+    /// <param name="Status">Registration status</param>
     /// <param name="SourceMessage">Message from parser(logging purpose)</param>
     /// <param name="OpenFrom">Date of registration opening</param>
     /// <param name="OpenUntil">Date of registration closing</param>
     public sealed record RegistrationInfo(
         
-        RegistrationStatus status,
+        RegistrationStatus Status,
 
         string? SourceMessage = null,
 
---
+++ src/RegistrationMonitor.Infrastructure/Trackers/DummyRegistrationTracker.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using RegistrationMonitor.Core.Interfaces;
+using RegistrationMonitor.Core.Models;
+
+namespace RegistrationMonitor.Infrastructure.Trackers
+{
+    public class DummyRegistrationTracker : IRegistrationTracker
+    {
+        private readonly ILogger<DummyRegistrationTracker> _logger;
+
+        private static readonly DateTimeOffset SimulatedOpenTime = DateTimeOffset.UtcNow.AddMinutes(2);
+
+        public DummyRegistrationTracker(ILogger<DummyRegistrationTracker> logger) 
+        {
+            _logger = logger;
+        }
+
+        public Task<RegistrationInfo> GetCurrentStatusAsync(CancellationToken token) 
+        {
+            var now = DateTimeOffset.UtcNow;
+            var isOpen = now >= SimulatedOpenTime;
+
+            _logger.LogDebug("[DummyTracker] Check simulation. Time: {Now:HH:mm:ss}." + 
+                "Registration opens at: {OpenTime:HH:mm:ss}. Status: {Status}",
+                now, SimulatedOpenTime, isOpen ? "OPEN" : "CLOSED");
+
+            var info = new RegistrationInfo(
+                Status: isOpen ? RegistrationStatus.Open : RegistrationStatus.Closed,
+                SourceMessage: isOpen ? 
+                    $"[Simulation] Registration opened at {SimulatedOpenTime:HH:mm:ss} UTC" : 
+                    $"[Simulation] Waiting. Registration will be open at {SimulatedOpenTime:HH:mm:ss} UTC",
+                OpenFrom: isOpen ? SimulatedOpenTime : null );
+
+            return Task.FromResult(info);
+        }
+    }
+}
