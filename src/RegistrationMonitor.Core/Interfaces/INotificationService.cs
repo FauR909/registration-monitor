d16b766ea32f651a29f4ee9f93a32670bb0feb07
Oleh Filyk <filykoleg2102@gmail.com>
2026-06-15 19:00:58 +0300

    feat(core):define IRegistrationTracker and INotificationService interfaces

---
+++ src/RegistrationMonitor.Core/Interfaces/INotificationService.cs
@@ -0,0 +1,23 @@
+﻿using RegistrationMonitor.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistrationMonitor.Core.Interfaces
+{
+    /// <summary>
+    /// Service for notifications(Telegram / Email)
+    /// </summary>
+    public interface INotificationService
+    {
+        /// <summary>
+        /// Send "registration open" notification
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        Task SendRegistrationOpenedAsync(RegistrationInfo info, CancellationToken token = default);
+    }
+}
