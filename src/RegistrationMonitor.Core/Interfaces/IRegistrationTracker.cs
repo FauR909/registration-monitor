c2cd28c2ef9388b32f48f08631172590de9f910b
Oleh Filyk <filykoleg2102@gmail.com>
2026-06-15 19:10:33 +0300

    Feature/phase-1-core interfaces (#1)
    * feat(core): add RegistrationInfo record and RegistrationStatus enum
    
    * feat(core): define IRegistrationTracker and INotificationService interfaces

---
+++ src/RegistrationMonitor.Core/Interfaces/IRegistrationTracker.cs
@@ -0,0 +1,19 @@
+﻿using RegistrationMonitor.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistrationMonitor.Core.Interfaces
+{
+    public interface IRegistrationTracker
+    {
+        /// <summary>
+        /// Asynchronously checks registration status
+        /// </summary>
+        /// <param name="token">Cancellation token</param>
+        /// <returns><see cref="RegistrationInfo"/> with current status</returns>
+        Task<RegistrationInfo> GetCurrentStatusAsync(CancellationToken token = default);
+    }
+}
---
+++ src/RegistrationMonitor.Core/Models/RegistrationInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistrationMonitor.Core.Models
+{
+    /// <summary>
+    /// Result of checking registration status
+    /// </summary>
+    /// <param name="status">Registration status</param>
+    /// <param name="SourceMessage">Message from parser(logging purpose)</param>
+    /// <param name="OpenFrom">Date of registration opening</param>
+    /// <param name="OpenUntil">Date of registration closing</param>
+    public sealed record RegistrationInfo(
+        
+        RegistrationStatus status,
+
+        string? SourceMessage = null,
+
+        DateTimeOffset? OpenFrom = null,
+        
+        DateTimeOffset? OpenUntil = null
+    );
+}
