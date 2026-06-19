16ca25ff778516877243cb5444b79e713edc3745
Oleh Filyk <filykoleg2102@gmail.com>
2026-06-19 15:40:41 +0300

    feat(infrastructure/core): implement TelegramNotificationService with Kyiv time display through DateTimeOffset extension

---
+++ src/RegistrationMonitor.Core/Extensions/DateTimeOffsetExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegistrationMonitor.Core.Extensions
+{
+    public static class DateTimeOffsetExtensions
+    {
+        private static readonly TimeZoneInfo KyivTimeZone =
+        TimeZoneInfo.FindSystemTimeZoneById("Europe/Kyiv");
+
+        public static DateTimeOffset ToKyivTime(this DateTimeOffset utcTime) =>
+        TimeZoneInfo.ConvertTime(utcTime, KyivTimeZone);
+    }
+}
