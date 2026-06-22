860d0f832b94dd4cf638958f16e72cc7709226d7
Oleh Filyk <filykoleg2102@gmail.com>
2026-06-22 14:54:57 +0300

    refactor(infrastructure): introduce AppPaths for Docker-volume-friendly data directory

---
+++ src/RegistrationMonitor.Infrastructure/AppPaths.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegistrationMonitor.Infrastructure
+{
+    public static class AppPaths
+    {
+        public static string DataDirectory { get; }
+
+        static AppPaths()
+        {
+            DataDirectory = Path.Combine(AppContext.BaseDirectory, "data");
+            Directory.CreateDirectory(DataDirectory);
+        }
+    }
+}
