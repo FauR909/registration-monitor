1311e543b71f7525ac6e17692725628588ad4138
Oleh Filyk <filykoleg2102@gmail.com>
2026-06-23 12:49:50 +0300

    feat(core): add Subscriber entity and ISubscriberRepository interface

---
+++ src/RegistrationMonitor.Core/Interfaces/ISubscriberRepository.cs
@@ -0,0 +1,17 @@
+﻿using RegistrationMonitor.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace RegistrationMonitor.Core.Interfaces
+{
+    public interface ISubscriberRepository
+    {
+        Task<IReadOnlyList<Subscriber>> GetActiveSubscribersAsync(CancellationToken token = default);
+
+        Task AddOrActivateAsync(long chatId, string? username, CancellationToken token = default);
+
+        Task DeactivateAsync(long chatId, CancellationToken token = default);
+    }
+}
